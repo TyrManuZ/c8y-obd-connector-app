08beb1e21c3327f3984cfb1030a634fb714b1121
Tobias Sommer <tobias.sommer@cumulocity.com>
2018-09-01 21:46:24 +0200

    update license information

--- app/src/main/java/com/cumulocity/obdconnector/messages/impl/RpmMessage.java
+++ app/src/main/java/com/cumulocity/obdconnector/messages/impl/RpmMessage.java
@@ -1,3 +1,8 @@
+/*
+ * Copyright (c) 2018. Tobias Sommer
+ * Licensed under the MIT License. See LICENSE file in the project root for full license information.
+ */
+
 package com.cumulocity.obdconnector.messages.impl;
 
 import com.github.pires.obd.commands.engine.RPMCommand;
--- app/src/main/java/com/cumulocity/obdconnector/mqtt/CumulocityConfiguration.java
+++ app/src/main/java/com/cumulocity/obdconnector/mqtt/CumulocityConfiguration.java
@@ -1,3 +1,8 @@
+/*
+ * Copyright (c) 2018. Tobias Sommer
+ * Licensed under the MIT License. See LICENSE file in the project root for full license information.
+ */
+
 package com.cumulocity.obdconnector.mqtt;
 
 public class CumulocityConfiguration {
